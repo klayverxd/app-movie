54b4e8ea76b87c7f00fb5d586fd76ef183764d7e
klayverxd <klayverx@gmail.com>
2021-09-07 12:09:42 -0300

    att movieSaga

--- src/shared/store/sagas/index.js
+++ src/shared/store/sagas/index.js
@@ -1,7 +1,7 @@
 import { all } from 'redux-saga/effects'
 
-import userSaga from './userSaga'
+import movieSaga from './movieSaga'
 
 export default function* rootSaga() {
-  yield all([userSaga()])
+  yield all([movieSaga()])
 }
---
+++ src/shared/store/sagas/movieSaga.js
@@ -0,0 +1,22 @@
+import { call, put, takeEvery } from 'redux-saga/effects'
+
+import api from '../../../services/api'
+
+function getApi() {
+  return api.get('/3/movie/popular')
+}
+
+function* fetchMovie() {
+  try {
+    const movies = yield call(getApi)
+    yield put({ type: 'GET_MOVIE_SUCSESS', movies: movies.data.results })
+  } catch (error) {
+    yield put({ type: 'GET_MOVIE_FAILED', message: error.message })
+  }
+}
+
+function* movieSaga() {
+  yield takeEvery('GET_MOVIE_REQUESTED', fetchMovie)
+}
+
+export default movieSaga
