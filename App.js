2bcaf4f0a40ecc5d7c012be2dbfdc775fd345355
klayverxd <klayverx@gmail.com>
2021-09-07 21:32:59 -0300

    refact safe area view

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
 import React from 'react'
 
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, View } from 'react-native'
+import { SafeAreaView, StatusBar, StyleSheet, View } from 'react-native'
 
 import { ThemeProvider } from 'styled-components/native'
 import themes from './src/shared/constants/themes'
 
 import { Provider } from 'react-redux'
 import store from './src/shared/store'
+
 import Movies from './src/shared/components/Movies'
 
 export default function App() {
   return (
     <Provider store={store}>
       <ThemeProvider theme={themes}>
-        <View style={styles.container}>
-          <StatusBar style="auto" />
+        <SafeAreaView style={styles.container}>
+          <StatusBar />
 
           <Movies />
-        </View>
+        </SafeAreaView>
       </ThemeProvider>
     </Provider>
   )
@@ -28,7 +28,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 18,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 })
