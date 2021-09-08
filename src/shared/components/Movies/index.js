8746d801cc6e5010f21364e6b4852165ece75714
klayverxd <klayverx@gmail.com>
2021-09-07 21:32:33 -0300

    refact card movie

--- src/shared/components/Movies/index.js
+++ src/shared/components/Movies/index.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text, TextInput, Button, FlatList } from 'react-native'
+import React, { useEffect } from 'react'
+import { View, Text, ActivityIndicator, ScrollView, Image } from 'react-native'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { getMovie } from '../../store/action/movie'
+import SearchBar from '../SearchBar'
 
 export default function Movies() {
   const dispatch = useDispatch()
-  const [searchMovie, setSearchMovie] = useState('')
 
   const movies = useSelector(state => state.movie.movies)
   const loading = useSelector(state => state.movie.loading)
@@ -18,22 +18,27 @@ export default function Movies() {
 
   return (
     <View>
-      {loading && <Text>Loading...</Text>}
-      <TextInput
-        onChangeText={setSearchMovie}
-        value={searchMovie}
-        placeholder="Procurar filme"
-        style={{ backgroundColor: 'gray', padding: 10 }}
-      />
-      <Button onPress={() => console.log(searchMovie)} title="Mostrar search" />
-      {/* <FlatList
-        data={movies}
-        renderItem={renderMovie}
-        keyExtractor={movie => movie.id}
-      /> */}
-      {movies.map(movie => (
-        <Text key={movie.id}>{movie.title}</Text>
-      ))}
+      {loading && <ActivityIndicator size="large" color="#999" />}
+      <SearchBar />
+
+      <ScrollView
+        contentContainerStyle={{
+          alignItems: 'center',
+        }}
+      >
+        {movies.map(movie => (
+          <View style={{ flex: 1, margin: 12 }} key={movie.id}>
+            <Image
+              source={{
+                uri: `https://image.tmdb.org/t/p/w200${movie.poster_path}`,
+              }}
+              style={{ width: 120, height: 160 }}
+            />
+            <Text style={{ maxWidth: 120 }}>{movie.title}</Text>
+          </View>
+        ))}
+      </ScrollView>
+
       {error && !loading && <Text>{error}</Text>}
     </View>
   )
