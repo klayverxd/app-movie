55c3289e8bb581a780197d1dbb4c2d896d84c534
klayverxd <klayverx@gmail.com>
2021-09-07 22:41:56 -0300

    refact counter to fs

--- src/shared/store/reducer/index.js
+++ src/shared/store/reducer/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux'
 
-import counter from './counter'
+import fontSize from './fontSize'
 import movie from './movies'
 
 const allReducers = combineReducers({
-  counter,
+  fontSize,
   movie,
 })
 
