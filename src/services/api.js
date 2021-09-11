f458459976beb7f8806e75c592d7bed1de96e318
klayverxd <klayverx@gmail.com>
2021-09-10 22:55:29 -0300

    fetch movies functions

--- src/services/api.js
+++ src/services/api.js
@@ -7,4 +7,34 @@ const api = axios.create({
   },
 })
 
+export function getFilteredMovies(search) {
+  return api.get('/3/search/movie', {
+    params: { query: search },
+  })
+}
+
+export function getActionMovies() {
+  return api.get('/3/discover/movie', {
+    params: { with_genres: 28 },
+  })
+}
+
+export function getDocumentaryMovies() {
+  return api.get('/3/discover/movie', {
+    params: { with_genres: 99 },
+  })
+}
+
+export function getAnimationMovies() {
+  return api.get('/3/discover/movie', {
+    params: { with_genres: 16 },
+  })
+}
+
+export function getScienceFictionMovies() {
+  return api.get('/3/discover/movie', {
+    params: { with_genres: 878 },
+  })
+}
+
 export default api
