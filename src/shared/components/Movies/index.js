7c8f72a7580e8720f7080ab36bb283e859a97e5f
klayverxd <klayverx@gmail.com>
2021-09-10 22:57:00 -0300

    add movie sections

--- src/shared/components/Movies/index.js
+++ src/shared/components/Movies/index.js
@@ -1,16 +1,26 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { Text, ActivityIndicator, ScrollView, Image } from 'react-native'
+import { Text, ActivityIndicator } from 'react-native'
 
 import { getMovies } from '../../store/action/movie'
+
+import SectionMovies from '../SectionMovies'
 
 import * as S from './styles'
 
 export default function Movies() {
   const dispatch = useDispatch()
 
-  const movies = useSelector(state => state.movie.movies)
+  const actionMovies = useSelector(state => state.movie.actionMovies)
+  const animationMovies = useSelector(state => state.movie.animationMovies)
+  const documentaryMovies = useSelector(state => state.movie.documentaryMovies)
+  const scienceFictionMovies = useSelector(
+    state => state.movie.scienceFictionMovies
+  )
+  const filteredMovies = useSelector(state => state.movie.filteredMovies)
+  const hasFilter = useSelector(state => state.movie.hasFilter)
+  const hasMovies = useSelector(state => state.movie.hasMovies)
   const loading = useSelector(state => state.movie.loading)
   const error = useSelector(state => state.movie.error)
 
@@ -22,31 +32,29 @@ export default function Movies() {
     <S.Container>
       {loading && <ActivityIndicator size="large" color="#999" />}
 
-      {!!movies.length ? (
-        <S.ContainerSection>
-          <S.HeaderSection>
-            <S.TextHeaderSection>Most Popular</S.TextHeaderSection>
-          </S.HeaderSection>
-          <ScrollView
-            contentContainerStyle={{
-              alignItems: 'center',
-            }}
-            horizontal={true}
-          >
-            {movies.map(movie => (
-              <S.ContainerCard key={movie.id}>
-                <Image
-                  source={{
-                    uri: `https://image.tmdb.org/t/p/w200${movie.poster_path}`,
-                  }}
-                  style={{ width: 120, height: 160 }}
-                />
-                <S.MovieTitle numberOfLines={1}>{movie.title}</S.MovieTitle>
-              </S.ContainerCard>
-            ))}
-          </ScrollView>
-        </S.ContainerSection>
-      ) : (
+      {!hasFilter && !loading && (
+        <>
+          <SectionMovies sectionName="Action Movies" movies={actionMovies} />
+          <SectionMovies
+            sectionName="Animation Movies"
+            movies={animationMovies}
+          />
+          <SectionMovies
+            sectionName="Science Fiction Movies"
+            movies={scienceFictionMovies}
+          />
+          <SectionMovies
+            sectionName="Documentary Movies"
+            movies={documentaryMovies}
+          />
+        </>
+      )}
+
+      {hasFilter && hasMovies && !loading && (
+        <SectionMovies sectionName="Filtered Movies" movies={filteredMovies} />
+      )}
+
+      {hasFilter && !hasMovies && !loading && (
         <S.ErrorText>No results...</S.ErrorText>
       )}
 
--- src/shared/components/Movies/styles.js
+++ src/shared/components/Movies/styles.js
@@ -4,40 +4,6 @@ import TextComponent from '../Text'
 
 export const Container = styled.ScrollView``
 
-export const ContainerSection = styled.View`
-  margin-bottom: 12px;
-`
-
-export const HeaderSection = styled.View`
-  background-color: ${({ theme }) => theme.PRIMARY_HEADER_SECTION_COLOR};
-
-  height: 40px;
-
-  align-items: center;
-  justify-content: center;
-
-  margin-bottom: 12px;
-`
-
-export const TextHeaderSection = styled(TextComponent)`
-  color: #fff;
-
-  font-weight: bold;
-`
-
-export const ContainerCard = styled.View`
-  flex: 1;
-  margin-right: 12px;
-
-  align-items: center;
-`
-
-export const MovieTitle = styled(TextComponent)`
-  color: #454545;
-
-  max-width: 130px;
-`
-
 export const ErrorText = styled(TextComponent)`
   color: #454545;
 
