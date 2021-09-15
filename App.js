504d59df6c21e8bef90f8505c75b7a560233ff85
klayverxd <klayverx@gmail.com>
2021-09-15 09:28:21 -0300

    add persistor

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import React from 'react'
 
+import { PersistGate } from 'redux-persist/es/integration/react'
 import { Provider } from 'react-redux'
 
 import Home from './src/screens/Home'
 
-import store from './src/shared/store'
+import { persistor, store } from './src/shared/store'
+
+import Loading from './src/shared/components/Loading'
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Home />
+      <PersistGate
+        loading={<Loading text="Loading persistor..." />}
+        persistor={persistor}
+      >
+        <Home />
+      </PersistGate>
     </Provider>
   )
 }
--- src/shared/store/index.js
+++ src/shared/store/index.js
@@ -1,13 +1,26 @@
-import createSagaMiddleware from 'redux-saga'
 import { createStore, compose, applyMiddleware } from 'redux'
+
+import createSagaMiddleware from 'redux-saga'
+
+import { persistStore, persistReducer } from 'redux-persist'
+
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import allReducers from './reducer'
 import rootSaga from './sagas'
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+}
+
+const persistedReducer = persistReducer(persistConfig, allReducers)
+
 const sagaMiddleware = createSagaMiddleware(rootSaga)
 
-const store = compose(applyMiddleware(sagaMiddleware))(createStore)(allReducers)
+let composed = compose(applyMiddleware(sagaMiddleware))
+
+export let store = createStore(persistedReducer, composed)
+export let persistor = persistStore(store)
 
 sagaMiddleware.run(rootSaga)
-
-export default store
